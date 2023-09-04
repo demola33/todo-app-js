0b5074d86f79b28d07a38551ad346c9a4444587e
demola33 <akanmu.ademola2019@outlook.com>
2023-09-04 11:49:27 +0100

    Disable and enable button

--- js/app.js
+++ js/app.js
@@ -3,10 +3,15 @@
 const todoInput = document.querySelector(".header__todo-input");
 const todoSubmit = document.querySelector(".header__todo-submit");
 const todoList = document.querySelector(".header__todo-list");
+const addIcon = document.querySelector(".fa-plus");
+
+// Disable Input field
+setDisable();
 
 // EventListeners
 todoSubmit.addEventListener("click", addTodo);
 todoList.addEventListener("click", deleteCheck);
+todoInput.addEventListener("input", updateValue);
 
 // Functions
 class Item {
@@ -14,6 +19,26 @@ class Item {
     this.id = id;
     this.value = value;
     this.status = status;
+  }
+}
+
+function setDisable() {
+  todoSubmit.disabled = true;
+  addIcon.style.color = `#fff`;
+}
+
+function setEnable() {
+  todoSubmit.disabled = false;
+  addIcon.style.color = `#4D00D9`;
+}
+
+function updateValue(event) {
+  const inputValue = event.target.value;
+  console.log(inputValue === "");
+  if (inputValue !== "") {
+    setEnable();
+  } else {
+    setDisable();
   }
 }
 
@@ -25,48 +50,45 @@ function addTodo(event) {
   // Prevent form from submitting
   event.preventDefault();
 
+  // Get the user input value
   const inputValue = todoInput.value;
 
-  if (inputValue === "") {
-    console.log("Empty");
-  } else {
-    id++;
-    var item = new Item(id, inputValue, itemStatus[0]);
+  id++;
+  var item = new Item(id, inputValue, itemStatus[0]);
 
-    console.log(item);
-    // Create Todo div
-    const todoDiv = document.createElement("div");
-    todoDiv.classList.add("header__todo");
-    todoDiv.id = id;
+  console.log(item);
+  // Create Todo div
+  const todoDiv = document.createElement("div");
+  todoDiv.classList.add("header__todo");
+  todoDiv.id = id;
 
-    // Create Todo List item
-    const newTodoItem = document.createElement("li");
-    newTodoItem.innerText = inputValue;
-    newTodoItem.classList.add("header__todo-item");
-    todoDiv.appendChild(newTodoItem);
+  // Create Todo List item
+  const newTodoItem = document.createElement("li");
+  newTodoItem.innerText = inputValue;
+  newTodoItem.classList.add("header__todo-item");
+  todoDiv.appendChild(newTodoItem);
 
-    // Create Completed Button
-    const completedButton = document.createElement("button");
-    completedButton.innerHTML = '<i class="fa-solid fa-check"></i>';
-    completedButton.classList.add("complete-btn");
-    completedButton.classList.add("btn");
-    completedButton.classList.add("btn-success");
-    todoDiv.appendChild(completedButton);
+  // Create Completed Button
+  const completedButton = document.createElement("button");
+  completedButton.innerHTML = '<i class="fa-solid fa-check"></i>';
+  completedButton.classList.add("complete-btn");
+  completedButton.classList.add("btn");
+  completedButton.classList.add("btn-success");
+  todoDiv.appendChild(completedButton);
 
-    // Create Delete Button
-    const deleteButton = document.createElement("button");
-    deleteButton.innerHTML = '<i class="fa-solid fa-trash"></i>';
-    deleteButton.classList.add("trash-btn");
-    deleteButton.classList.add("btn");
-    deleteButton.classList.add("btn-danger");
-    todoDiv.appendChild(deleteButton);
+  // Create Delete Button
+  const deleteButton = document.createElement("button");
+  deleteButton.innerHTML = '<i class="fa-solid fa-trash"></i>';
+  deleteButton.classList.add("trash-btn");
+  deleteButton.classList.add("btn");
+  deleteButton.classList.add("btn-danger");
+  todoDiv.appendChild(deleteButton);
 
-    // Append Todo div to todoList
-    todoList.appendChild(todoDiv);
+  // Append Todo div to todoList
+  todoList.appendChild(todoDiv);
 
-    // Clear Todo Input Value
-    todoInput.value = "";
-  }
+  // Clear Todo Input Value
+  todoInput.value = "";
 }
 
 function deleteCheck(event) {
@@ -76,7 +98,6 @@ function deleteCheck(event) {
   // On Delete
   if (item.classList[0] === "trash-btn") {
     const todo = item.parentElement;
-    todo.classList.add("fall");
     todo.remove();
   }
 
